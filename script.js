787dc790f4efa7a57969f3c19ca91bf2b772c31e
alokkumax <alokkumax1@gmail.com>
2023-08-07 19:20:10 +0530

    added tabs

--- script.js
+++ script.js
@@ -1,6 +1,81 @@
-document.querySelector("body").classList.add(" light");
 function comingSoon(){
     // $(".content").html("Coming Soon")
     alert("Coming Soon")
 
 }
+/// BTN 1
+    document.querySelector("#btn1").addEventListener("click", function(){
+    //show 1
+    document.querySelector(".contentB").style.display = "block"
+    
+    //active 1 
+    this.classList.replace("button","active");
+    
+    //remove 2 n 3 n 4
+    document.querySelector("#btn2").classList.replace("active","button");
+    document.querySelector("#btn3").classList.replace("active","button");
+    document.querySelector("#btn4").classList.replace("active","button");
+    
+    // hide 2 n 3 n 4
+    document.querySelector(".contentV").style.display = "none"
+    document.querySelector(".contentR").style.display = "none"
+    document.querySelector(".contentC").style.display = "none"
+    })
+    
+//BTN 2
+    document.querySelector("#btn2").addEventListener("click", function(){
+    //show 2
+    document.querySelector(".contentV").style.display = "block"
+    //active 2 
+    this.classList.replace("button","active");
+    
+    //remove 1 n 3 n 4
+    document.querySelector("#btn1").classList.replace("active","button");
+    document.querySelector("#btn3").classList.replace("active","button");
+    document.querySelector("#btn4").classList.replace("active","button");
+    
+    // hide 1 n 3 n 4
+    document.querySelector(".contentB").style.display = "none" 
+    document.querySelector(".contentR").style.display = "none"
+    document.querySelector(".contentC").style.display = "none"
+    
+    })
+    
+/// BTN 3
+    document.querySelector("#btn3").addEventListener("click", function(){
+    //show 3
+    document.querySelector(".contentR").style.display = "block"
+    
+    //active 3 
+    this.classList.replace("button","active");
+    
+    //remove 1 n 2 n 4
+    document.querySelector("#btn1").classList.replace("active","button");
+    document.querySelector("#btn2").classList.replace("active","button");
+    document.querySelector("#btn4").classList.replace("active","button");
+    
+    //hide 1 n 2 n 4
+    document.querySelector(".contentB").style.display = "none"
+    document.querySelector(".contentV").style.display = "none"
+    document.querySelector(".contentC").style.display = "none"
+    
+    })
+/// BTN 4
+    document.querySelector("#btn4").addEventListener("click", function(){
+        //show 3
+        document.querySelector(".contentC").style.display = "block"
+        
+        //active 3 
+        this.classList.replace("button","active");
+        
+        //remove 1 n 2 n 3
+        document.querySelector("#btn1").classList.replace("active","button");
+        document.querySelector("#btn2").classList.replace("active","button");
+        document.querySelector("#btn3").classList.replace("active","button");
+        
+        //hide 1 n 2 n 3
+        document.querySelector(".contentB").style.display = "none"
+        document.querySelector(".contentV").style.display = "none"
+        document.querySelector(".contentR").style.display = "none"
+        
+        })
